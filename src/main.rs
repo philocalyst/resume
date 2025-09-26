885f178bc2ba000c893c1bf7151409e8b83128d7
Myles Wirth <regime_skylark.0y@icloud.com>
2025-09-26 15:11:47 -0400

    initial files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#![recursion_limit = "512"]
+
+use html::content::Section;
+use json_resume;
+
+trait HTMLResume {
+    fn build_header() -> Section;
+    fn build_interests() -> Section;
+    fn build_languages() -> Section;
+    fn build_references() -> Section;
+    fn build_section() -> Section;
+    fn build_skills() -> Section;
+    fn build_summary() -> Section;
+}
+
 fn main() {
     println!("Hello, world!");
 }
